9da03c92072e756d520d83871ddda01e933b2079
Harsh Shandilya <me@msfjarvis.dev>
2020-10-12 17:17:58 +0530

    Test reading git repo metadata
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use git2::Repository;
 use std::path::Path;
 use walkdir::WalkDir;
 
@@ -7,18 +8,33 @@ fn main() {
         None => {
             println!("Usage:\n  gitice <dir>\n");
             return;
-        },
+        }
     };
     let mut items: Vec<String> = Vec::new();
-    for entry in WalkDir::new(dir)
-        .into_iter()
-        .filter_map(|e| e.ok())
-    {
+    for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
         if entry.file_type().is_dir() {
             let path = format!("{}/.git", entry.path().display());
             let git_dir = Path::new(&path);
             if git_dir.exists() {
-                items.push(entry.path().to_string_lossy().to_string());
+                let repo = match Repository::open(git_dir) {
+                    Ok(repo) => repo,
+                    Err(e) => {
+                        println!("Failed to open repository: {}", e);
+                        return;
+                    }
+                };
+                let head = match repo.head() {
+                    Ok(head) => head,
+                    Err(e) => {
+                        println!("Failed getting repository head: {}", e);
+                        return;
+                    }
+                };
+                items.push(format!(
+                    "{} = {}",
+                    entry.path().to_string_lossy().to_string(),
+                    head.name().unwrap()
+                ));
             }
         };
     }
