820fc6f1ab672249ca33d4ea26267d3946e5fdd7
Harsh Shandilya <me@msfjarvis.dev>
2020-10-12 17:11:40 +0530

    Add an initial search mechanism
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,26 @@
+use std::path::Path;
+use walkdir::WalkDir;
+
 fn main() {
+    let dir = match std::env::args().nth(1) {
+        Some(d) => d,
+        None => {
+            println!("Usage:\n  gitice <dir>\n");
+            return;
+        },
+    };
+    let mut items: Vec<String> = Vec::new();
+    for entry in WalkDir::new(dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        if entry.file_type().is_dir() {
+            let path = format!("{}/.git", entry.path().display());
+            let git_dir = Path::new(&path);
+            if git_dir.exists() {
+                items.push(entry.path().to_string_lossy().to_string());
+            }
+        };
+    }
+    println!("{:#x?}", items);
 }
